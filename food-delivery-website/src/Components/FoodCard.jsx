62efb1da8b9a352bfde0930130b8a1af22b234bd
Bipin Babu <bipinbabu@live.com>
2020-06-13 13:33:01 +0530

    Added pagination for individual items

---
+++ food-delivery-website/src/Components/FoodCard.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import style from "./DisplayStyles.module.css";
+
+class FoodCard extends Component{
+    constructor(props){
+        super(props);
+    }
+
+
+
+    render(){
+        const {food} = this.props;
+        const name = food.name;
+        const descr = food.descr;
+        const rate = food.rate
+        return(
+            <div className="col-12 col-md-4">
+                <div className="card" style={{width:"22rem",border:"1px solid black",marginRight:12,marginBottom:20}}>
+                    <div className="card-body">
+                        <h5 className={`card-title text-dark text-center ${style.cardTitle}`}>{name}</h5>
+                        <div className={`d-flex flex-column`} style={{justifyContent:"space-between",textAlign:"center"}}>
+                            <p className={`card-text text-muted ${style.cardType}`}>{`${descr}`}</p>
+                            <p className={`card-text ${style.cardType}`}>{`${rate}`}</p>
+                            
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
+export default FoodCard;
--- food-delivery-website/src/Components/Restaurant.jsx
+++ food-delivery-website/src/Components/Restaurant.jsx
@@ -1,25 +1,61 @@
 import React, { Component } from "react";
 import style from "./styles.module.css";
-// import data from  "./AllRestaurants.json";
+import foodData from "./AllFood.json";
+import { v4 as uuidv4 } from 'uuid';
+import FoodCard from "./FoodCard";
+import Pagination from "./Pagination";
 
 class Restaurant extends Component{
     constructor(props){
         super(props);
+        this.state={
+            foodActive:1
+        }
+    }
+    foodPaginate = (number)=>{
+        this.setState({
+            foodActive:number
+        })
     }
 
     render(){
+        
         console.log("Inside Restaurants Page");
-        const {match} = this.props;
+        const {match,data} = this.props;
         console.log(match);
+        console.log("data received in restaurants.jsx is",data);
         const matchedName = match.params.name;
+
+        const productObj = data.find(element => element.name == matchedName);
+        console.log("matched obj is",productObj);
+
+
+        let perPage = 10;
+        let foodActivePage = this.state.foodActive;
+        const totalTopRest = foodData != null? foodData.length:0;
         return(
             <>
-                <div className={`${style.body} text-center`} style={{marginTop:"0vh",border:"1px solid black"}}>
-                    <h1 style={{textAlign:"center"}}>Restaurant Page</h1>
-                    <div className={`row`}>
+                <div className={`${style.body}`} style={{marginTop:"0vh"}}>
+                    <h1 style={{textAlign:"center"}}>{productObj.name}</h1>
+                    <div style={{textAlign:"center"}}>
+                        <img src={productObj.url} style={{maxWidth:"100%",maxHeight:"50%"}}></img>
+                        <hr/>
+                    </div>
+                    <div className={`row`} style={{marginTop:30}}>
                         <div className={`col-1`}></div>
                         <div className={`col-10`}>
-                            Here is the data displayed
+                            <div>Picked for you</div>
+                            <div className="row">
+                            {foodData.filter(item=>item.restaurant == matchedName)
+                            .filter((item,index)=> index >= (foodActivePage - 1)*perPage && index < foodActivePage * perPage)
+                            .map(item=>
+                                <FoodCard food = {item} key={uuidv4()}/>
+                            )
+                            }
+                            </div>
+                            <div>
+                                <Pagination perPage={perPage} totalPosts = {totalTopRest} paginate={this.foodPaginate} className="mx-auto"/>
+                            </div>
                         </div>
                         <div className={`col-1`}></div>
                     </div>
